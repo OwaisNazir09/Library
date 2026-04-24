11ac2bacf70a01efbdbe3f1db63a9d042de2ebf3
OwaisNazir09 <owaisaalam38@gmail.com>
2026-04-24 21:46:44 +0530

    --

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3245/api',
+  baseURL: 'https://library-7qme.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
